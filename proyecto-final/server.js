d5fec627b0242be159a400d9f80e2173d059ca95
Alberto Ramos <bertoramos2001@gmail.com>
2020-06-18 20:47:27 +0200

    server con endpoints videos familia

--- proyecto-final/server.js
+++ proyecto-final/server.js
@@ -4,10 +4,14 @@ const bodyParser = require('body-parser');
 const cors = require('cors')
 const express = require('express');
 const morgan = require('morgan');
+const multer = require('multer')
+
+const upload = multer({dest: 'uploads/'})
 
 const { listUsers, login, registerFamily, registerScout } = require('./controllers/users')
 const { isAuthenticated, canUpdateProfile } = require('./middlewares/auth')
 const { modifyProfileFamily, modifyProfileScout, showProfile } = require('./controllers/profiles')
+const { postVideo } = require('./controllers/videos')
 
 const port = process.env.PORT;
 const app = express();
@@ -18,15 +22,16 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(cors());
 
 //registro y login
-app.post('/registroFamilia', registerFamily);
-app.post('/registroOjeador', registerScout);
+app.post('/registroFamilia', upload.single('avatarPerfil'), registerFamily);
+app.post('/registroOjeador', upload.single('avatarPerfil'), registerScout);
 app.post('/login', login);
 app.get('/registroOjeador', isAuthenticated, listUsers);
 //ver perfil público
 app.get('/perfil/:role/:email', showProfile);
-app.put('/perfil/editar/familia/:email', isAuthenticated, canUpdateProfile, modifyProfileFamily);
-app.put('/perfil/editar/ojeador/:email', isAuthenticated, canUpdateProfile, modifyProfileScout);
-//app.put('/perfil/:role/:email', canChangeProfile, modifyProfile, showProfile)
+app.put('/perfil/editar/familia/:email', isAuthenticated, canUpdateProfile, upload.single('avatarPerfil'), modifyProfileFamily);
+app.put('/perfil/editar/ojeador/:email', isAuthenticated, canUpdateProfile, upload.single('avatarPerfil'), modifyProfileScout);
+app.post('/perfil/editar/familia/:email/videos', upload.single('videoFamilia'))
+//app.get('/perfil/familia/:email/videos', showVideos)
 
 
 app.use((error, req, res, next) => {  //middleware generico para la gestion de errores (si algun middleware da error, se ejecuta este)
