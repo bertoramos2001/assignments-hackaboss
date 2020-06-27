15ad30d8b13fe408cd0ee25b605fc6c91620685e
Alberto Ramos <bertoramos2001@gmail.com>
2020-06-27 21:42:15 +0200

    arreglado error que permitia borrar videos de otro

--- proyecto-final/controllers/videos.js
+++ proyecto-final/controllers/videos.js
@@ -4,6 +4,14 @@ const postVideo = (req, res, next) => {
     let videoFamilia = '';
     const { titulo, descripcion } = req.body;
     const { email } = req.params;
+    const user = bd.getUser(email);
+
+    if (!user) {
+        const userNotFoundError = new Error('usuario no encontrado');
+        userNotFoundError.status = 404;
+        next(userNotFoundError)
+        return;
+    }
 
     if (!req.file) {
         const profileImageError = new Error('Debes añadir un vídeo');
@@ -12,7 +20,7 @@ const postVideo = (req, res, next) => {
     } else {
         videoFamilia = req.file.path;
     }
-    const id = (bd.getUser(email)).id;
+    const id = user.id;
     bd.saveVideo(id, titulo, descripcion, videoFamilia);
     res.json(videoFamilia);
 }
@@ -32,8 +40,33 @@ const showVideos = (req, res, next) => {
 }
 
 const deleteVideo = (req, res, next) => {
-    let { idVideo } = req.params;
-    bd.deleteVideo(parseInt(idVideo));
+    const { idVideo, email } = req.params;
+    const user = bd.getUser(email);
+
+    if (!user) {
+        const userNotFoundError = new Error('usuario no encontrado');
+        userNotFoundError.status = 404;
+        next(userNotFoundError)
+        return;
+    }
+
+    const videoToDelete = bd.getVideo(idVideo);
+
+    if (!videoToDelete) {
+        const videoError = new Error(`No existe el video que estas buscando`);
+        videoError.status = 404;
+        next(videoError);
+        return;
+    }
+
+    if (parseInt(videoToDelete.id) !== parseInt(user.id)) {
+        const videoError = new Error(`No tienes permiso para borrar un video que no es tuyo`);
+        videoError.status = 403;
+        next(videoError);
+        return;
+    }
+
+    bd.deleteVideo(idVideo);
     res.send();
 }
 
