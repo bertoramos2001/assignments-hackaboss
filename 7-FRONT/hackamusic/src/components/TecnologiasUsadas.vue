0368168114305abd84146a5b7a8131810d4d0699
Alberto Ramos <bertoramos2001@gmail.com>
2020-07-18 23:04:00 +0200

    Entrega vue 1

---
+++ 7-FRONT/hackamusic/src/components/TecnologiasUsadas.vue
@@ -0,0 +1,41 @@
+<template>
+  <div>
+      <h2>¡Hola! Mi nombre es Alberto Ramos y he hecho esta plataforma utilizando las siguientes tecnologías:</h2>
+      <ul>
+          <li>
+              <h3>💠1. Vue.js💠</h3>
+          </li>
+          <p>Este es el framework con el que trabajamos el apartado de front en el bootcamp de Hack a Boss, por lo que fue el que empleé.</p>
+          <li>
+              <h3>🔶2. API de Lastfm🔶</h3>
+              <p>Esta es la API utilizada en este proyecto, con la que obtenemos tanto los artistas como las canciones o etiquetas más escuchadas en España.</p>
+          </li>
+          <li>
+              <h3>🔰3. librería AXIOS🔰</h3>
+              <p>Librería empleada para realizar peticiones a la API y así poder obtener los datos previamente mencionados de esta.</p>
+          </li>
+          <li>
+              <h3>🔆4. Librería Vue-headful🔆</h3>
+              <p>Librería empleada para añadir información a cada página de la web. En este caso fue empleada para añadir títulos a cada vista.</p>
+          </li>
+      </ul>
+  </div>
+</template>
+
+<script>
+export default {
+    name: 'TecnologiasUsadas'
+}
+</script>
+
+<style scoped>
+div {
+    padding: 0 15px 0 15px;
+}
+ul {
+    list-style: none;
+}
+li {
+        font-size: 130%
+}
+</style>
--- 7-FRONT/hackamusic/src/views/About.vue
+++ 7-FRONT/hackamusic/src/views/About.vue
@@ -2,8 +2,21 @@
   <div class="about">
     <vue-headful title="About"/>
     <h1>👉 Tecnologías utilizadas en esta entrega 👈</h1>
+    <tecnologiasusadas/>
   </div>
 </template>
+
+<script>
+//Importamos el componente de lista de tags más escuchados
+import tecnologiasusadas from '@/components/TecnologiasUsadas.vue'
+
+export default {
+  name: 'About',
+  components: {
+    tecnologiasusadas
+  }
+}
+</script>
 
 <style scoped>
 h1 {
